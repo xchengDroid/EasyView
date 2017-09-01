9344cc92da9738715d1233c036104c36b3238bfa
xchengDroid <543174222@qq.com>
2017-09-01 13:48:55 +0800

    添加文案，修改TabInfo构造方法

--- library/src/main/java/com/xcheng/view/adapter/TabInfo.java
+++ library/src/main/java/com/xcheng/view/adapter/TabInfo.java
@@ -4,42 +4,50 @@ import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 
 public final class TabInfo {
-    public CharSequence title;
-    public int iconResId = 0;
-    /**
-     * 指示器的背景
-     */
-    public int bgResId = 0;
-
+    public final CharSequence title;
     public final String tag;
     public final Class<?> clazz;
     public final Bundle args;
 
-    public TabInfo(String _tag, CharSequence _title, Class<?> _clazz, Bundle bundle) {
+    public final int iconResId;
+    /**
+     * Tab背景
+     */
+    public final int bgResId;
+
+    public TabInfo(String _tag, @DrawableRes int _iconResId, CharSequence _title, @DrawableRes int _bgResId, Class<?> _clazz, Bundle bundle) {
         tag = _tag;
         title = _title;
         clazz = _clazz;
+        iconResId = _iconResId;
+        bgResId = _bgResId;
         args = bundle != null ? bundle : new Bundle();
         args.putString("tag", _tag);
         args.putCharSequence("title", _title);
     }
 
+    public TabInfo(String _tag, CharSequence _title, Class<?> _clazz, Bundle _args) {
+        this(_tag, 0, _title, 0, _clazz, _args);
+    }
+
     public TabInfo(String _tag, CharSequence _title, Class<?> _clazz) {
-        this(_tag, _title, _clazz, null);
+        this(_tag, 0, _title, 0, _clazz, null);
     }
-
+    /**
+     * 如果没有DrawableRes 传入 0
+     *
+     * @param _iconResId icon资源
+     */
     public TabInfo(String _tag, @DrawableRes int _iconResId, CharSequence _title, Class<?> _clazz) {
-        this(_tag, _title, _clazz);
-        iconResId = _iconResId;
+        this(_tag, _iconResId, _title, 0, _clazz, null);
     }
 
-    public TabInfo(String _tag, CharSequence _title, Class<?> _clazz, @DrawableRes int _bgResId) {
-        this(_tag, _title, _clazz);
-        bgResId = _bgResId;
-    }
-
-    public TabInfo(String _tag, CharSequence _title, Class<?> _clazz, @DrawableRes int _bgResId, Bundle bundle) {
-        this(_tag, _title, _clazz, bundle);
-        bgResId = _bgResId;
+    /**
+     * 如果没有DrawableRes 传入 0
+     *
+     * @param _bgResId 背景资源
+     */
+    public TabInfo(String _tag, CharSequence _title, @DrawableRes int _bgResId, Class<?> _clazz) {
+        this(_tag, 0, _title, _bgResId, _clazz, null);
     }
 }
