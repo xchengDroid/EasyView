0c7f608eb44c1f432dc8f3812824d58698328578
xchengDroid <543174222@qq.com>
2017-09-02 08:57:19 +0800

    设置不渲染Divider最后一项

--- library/src/main/java/com/xcheng/view/adapter/DividerDecoration.java
+++ library/src/main/java/com/xcheng/view/adapter/DividerDecoration.java
@@ -1,19 +1,31 @@
-package com.xcheng.view.controller.dialog;
+package com.xcheng.view.adapter;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.annotation.ColorInt;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class OptionDecoration extends RecyclerView.ItemDecoration {
+public class DividerDecoration extends RecyclerView.ItemDecoration {
     private int mDividerColor;
     private int mDividerHeight;
+    private boolean hasEndDivider;
     private Paint mPaint;
 
-    public OptionDecoration(int dividerColor, int dividerHeight) {
+    public DividerDecoration(@ColorInt int dividerColor, int dividerHeight) {
+        this(dividerColor, dividerHeight, false);
+    }
+
+    /**
+     * @param dividerColor  分割线颜色
+     * @param dividerHeight 分割线高度
+     * @param hasEndDivider 最后一项是否有分割线
+     */
+    public DividerDecoration(@ColorInt int dividerColor, int dividerHeight, boolean hasEndDivider) {
         this.mDividerColor = dividerColor;
         this.mDividerHeight = dividerHeight;
+        this.hasEndDivider = hasEndDivider;
         init();
     }
 
@@ -27,17 +39,19 @@ public class OptionDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         int position = parent.getChildAdapterPosition(view);
-        if (position != parent.getAdapter().getItemCount() - 1) {
+        if (hasEndDivider || position != parent.getAdapter().getItemCount() - 1) {
             outRect.set(0, 0, 0, mDividerHeight);
         }
     }
 
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        final int count = parent.getChildCount();
-        for (int i = 0; i < parent.getChildCount(); i++) {
+        final int childCount = parent.getChildCount();
+        final int itemCount = parent.getAdapter().getItemCount();
+        for (int i = 0; i < childCount; i++) {
             View view = parent.getChildAt(i);
-            if (i < count - 1)
+            int position = parent.getChildAdapterPosition(view);
+            if (hasEndDivider || position != itemCount - 1)
                 c.drawLine(0, view.getBottom() + mDividerHeight / 2, parent.getRight(), view.getBottom() + mDividerHeight / 2, mPaint);
         }
     }
