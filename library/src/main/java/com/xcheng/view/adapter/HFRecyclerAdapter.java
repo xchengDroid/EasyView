c77d2600d3d1f11c724efd7af40f93d50f38f334
xchengDroid <543174222@qq.com>
2017-09-06 16:23:37 +0800

    继承EasyAdapter基础代码

--- library/src/main/java/com/xcheng/view/adapter/HFRecyclerAdapter.java
+++ library/src/main/java/com/xcheng/view/adapter/HFRecyclerAdapter.java
@@ -18,14 +18,18 @@ package com.xcheng.view.adapter;
 import android.content.Context;
 import android.support.annotation.IntRange;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.xcheng.view.util.EasyPreconditions;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -37,7 +41,12 @@ import java.util.List;
  * The usage of List<T> mData member is not mandatory. If you are going to provide your custom
  * implementation remember to override getItemCount method.
  */
-public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
+public abstract class HFRecyclerAdapter<T> extends RecyclerView.Adapter<EasyHolder> implements IAdapterDelegate<T, EasyHolder> {
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private final List<T> mData;
+    private final int mLayoutId;
+
     //-1为 INVALID_TYPE,不要设置为-1
     public static final int TYPE_HEADER = 0x00000111;
     public static final int TYPE_FOOTER = 0x00000222;
@@ -46,6 +55,9 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
     private View footerView;
     private View emptyView;
     private boolean showFooter = true;
+    /**
+     * 刷新加载每页的长度
+     */
     private final int mLength;
     /**
      * 标记是否绑定到RecyclerView
@@ -57,16 +69,171 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
     }
 
     public HFRecyclerAdapter(Context context, @LayoutRes int layoutId) {
+        this(context, layoutId, 10);
+    }
+
+    public HFRecyclerAdapter(Context context, @LayoutRes int layoutId, @IntRange(from = 1) int length) {
         this(context, null, layoutId, 10);
     }
 
-    public HFRecyclerAdapter(Context context, List<T> data, @LayoutRes int layoutId, @IntRange(from = 1) int length) {
-        super(context, data, layoutId);
+    public HFRecyclerAdapter(Context context, @Nullable List<T> data, @LayoutRes int layoutId) {
+        this(context, data, layoutId, 10);
+    }
+
+    public HFRecyclerAdapter(Context context, @Nullable List<T> data, @LayoutRes int layoutId, @IntRange(from = 1) int length) {
+        if (data == null) {
+            data = new ArrayList<>();
+        }
+        this.mData = data;
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mLayoutId = layoutId;
         mLength = length;
     }
 
+    public Context getContext() {
+        return mContext;
+    }
+
+    public LayoutInflater getInflater() {
+        return mInflater;
+    }
+
+    public View inflater(int layoutId, ViewGroup parent) {
+        return mInflater.inflate(layoutId, parent, false);
+    }
+
+    public final List<T> getData() {
+        return mData;
+    }
+
+    /**
+     * 获取mData数据的长度
+     *
+     * @return
+     */
+    public final int getDataCount() {
+        return mData.size();
+    }
+
+
     public int getLength() {
         return mLength;
+    }
+
+    /**
+     * mData数据是否为空
+     *
+     * @return
+     */
+    public final boolean isEmpty() {
+        return mData == null || mData.size() == 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /**
+     * @param position the  position in mData
+     * @return T
+     */
+    public T getItem(@IntRange(from = 0) int position) {
+        if (position >= 0 && position < mData.size()) {
+            return mData.get(position);
+        }
+        return null;
+    }
+
+    /**
+     * 获取在data数据列表上的位置
+     *
+     * @param adapterPosition onBindViewHolder 所指定的position
+     * @return position in mData
+     */
+    public int getPositionOfData(@IntRange(from = 0) int adapterPosition) {
+        return adapterPosition - getHeaderCount();
+    }
+
+    /**
+     * 刷新数据
+     */
+    public void refresh(@Nullable Collection<? extends T> newData) {
+        mData.clear();
+        if (newData != null && newData.size() > 0) {
+            /** 刷新数据 */
+            mData.addAll(newData);
+        }
+        notifyDataSetChanged();
+    }
+
+    /**
+     * notifyItemRangeInserted notifyItemRemoved notifyItemChanged 时必须要保持数据修改和操作的一致性，否则会越位时会报错，
+     * // boolean validateViewHolderForOffsetPosition(ViewHolder holder),每次都会检测
+     * if (holder.mPosition < 0 || holder.mPosition >= mAdapter.getItemCount()) {
+     * throw new IndexOutOfBoundsException("Inconsistency detected. Invalid view holder "
+     * + "adapter position" + holder);
+     * }
+     * <p>
+     * add new data to the end of mData
+     *
+     * @param loadMore the new data collection
+     */
+    public void addData(Collection<? extends T> loadMore) {
+        if (loadMore != null && loadMore.size() > 0) {
+            mData.addAll(loadMore);
+            notifyItemRangeInserted(getHeaderCount() + mData.size() - loadMore.size(), loadMore.size());
+        }
+    }
+
+    /**
+     * add new data in to certain locations
+     *
+     * @param position the position insert into mData
+     */
+    public void addData(@IntRange(from = 0) int position, Collection<? extends T> data) {
+        if (data != null && data.size() > 0) {
+            mData.addAll(position, data);
+            notifyItemInserted(getHeaderCount() + position);
+        }
+    }
+
+    public void add(T data) {
+        if (data != null) {
+            mData.add(data);
+            notifyItemInserted(getHeaderCount() + mData.size() - 1);
+        }
+    }
+
+    /**
+     * 追加数据
+     *
+     * @param position the  position insert into mData
+     * @param data     要追加的数据
+     */
+    public void add(@IntRange(from = 0) int position, T data) {
+        if (data != null) {
+            mData.add(position, data);
+            notifyItemInserted(getHeaderCount() + position);
+        }
+    }
+
+    /**
+     * 删除某条数据
+     *
+     * @param position the  position in mData
+     */
+    public void remove(@IntRange(from = 0) int position) {
+        if (position >= 0 && position < mData.size()) {
+            mData.remove(position);
+            notifyItemRemoved(getHeaderCount() + position);
+        }
+    }
+
+    public void remove(T item) {
+        int index = mData.indexOf(item);
+        remove(index);
     }
 
     /**
@@ -82,8 +249,15 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
         } else if (isFooterType(viewType)) {
             return new EasyHolder(footerView);
         } else {
-            return super.onCreateViewHolder(parent, viewType);
+            View itemView = getDelegateView(parent, viewType);
+            EasyPreconditions.checkState(itemView != null, "you can set a layoutId in construct method or override getItemView(parent,viewType) and return a NonNull itemView");
+            return new EasyHolder(itemView);
         }
+    }
+
+    @Override
+    public View getDelegateView(ViewGroup parent, int viewType) {
+        return mLayoutId != 0 ? inflater(mLayoutId, parent) : null;
     }
 
     /**
@@ -108,7 +282,8 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
                 mOnBindHolderListener.onBindFooter(holder);
             }
         } else {
-            super.onBindViewHolder(holder, position);
+            int positionOfData = getPositionOfData(position);
+            convert(holder, getItem(positionOfData), positionOfData);
         }
     }
 
@@ -121,16 +296,21 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
         } else if (isFooterPosition(position)) {
             return TYPE_FOOTER;
         }
-        return super.getItemViewType(position);
+        int positionOfData = getPositionOfData(position);
+        return getDelegateType(getItem(positionOfData), positionOfData);
     }
 
+    @Override
+    public int getDelegateType(T t, int position) {
+        return super.getItemViewType(position);
+    }
 
     @Override
     public int getItemCount() {
         return getDataCount() + getHeaderCount() + getFooterCount() + getEmptyCount();
     }
 
-    @Override
+
     public int getHeaderCount() {
         return hasHeader() ? 1 : 0;
     }
@@ -187,24 +367,6 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
         }
     }
 
-    public void notifyHeader() {
-        if (hasHeader()) {
-            notifyItemChanged(0);
-        }
-    }
-
-    public void notifyEmpty() {
-        if (hasEmpty()) {
-            notifyItemChanged(getHeaderCount());
-        }
-    }
-
-    public void notifyFooter() {
-        if (hasFooter()) {
-            notifyItemChanged(getItemCount() - 1);
-        }
-    }
-
     /**
      * Get footer data in this adapter, you should previously use {@link #setFooter}
      * in the adapter initialization code to set footer data.
@@ -229,6 +391,23 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
         }
     }
 
+    public void notifyHeader() {
+        if (hasHeader()) {
+            notifyItemChanged(0);
+        }
+    }
+
+    public void notifyEmpty() {
+        if (hasEmpty()) {
+            notifyItemChanged(getHeaderCount());
+        }
+    }
+
+    public void notifyFooter() {
+        if (hasFooter()) {
+            notifyItemChanged(getItemCount() - 1);
+        }
+    }
 
     /**
      * Call this method to show hiding footer.
@@ -403,5 +582,46 @@ public abstract class HFRecyclerAdapter<T> extends EasyRecyclerAdapter<T> {
         void onBindEmpty(EasyHolder holder);
 
         void onBindFooter(EasyHolder holder);
+    }
+
+    protected void clickToHolder(final EasyHolder easyHolder) {
+        easyHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(easyHolder, getPositionOfData(easyHolder.getAdapterPosition()));
+                }
+            }
+        });
+    }
+
+    protected void longClickToHolder(final EasyHolder easyHolder) {
+        easyHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return mOnItemLongClickListener != null &&
+                        mOnItemLongClickListener.onItemLongClick(easyHolder, getPositionOfData(easyHolder.getAdapterPosition()));
+            }
+        });
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(EasyHolder holder, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+    private OnItemLongClickListener mOnItemLongClickListener;
+
+    public interface OnItemLongClickListener {
+        boolean onItemLongClick(EasyHolder holder, int position);
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.mOnItemLongClickListener = onItemLongClickListener;
     }
 }
