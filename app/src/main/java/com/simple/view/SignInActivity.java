ca752e434f94a1c528b5b051aa8aedcd693c46e8
xchengDroid <543174222@qq.com>
2017-09-09 20:46:12 +0800

    修改theme主题

--- app/src/main/java/com/simple/view/SignInActivity.java
+++ app/src/main/java/com/simple/view/SignInActivity.java
@@ -1,23 +1,28 @@
 package com.simple.view;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.xcheng.view.controller.EasyActivity;
 import com.xcheng.view.processbtn.ActionProcessButton;
 
 
-public class SignInActivity extends Activity implements ProgressGenerator.OnCompleteListener {
+public class SignInActivity extends EasyActivity implements ProgressGenerator.OnCompleteListener {
 
     public static final String EXTRAS_ENDLESS_MODE = "EXTRAS_ENDLESS_MODE";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ac_sign_in);
 
+    @Override
+    public int getLayoutId() {
+        return R.layout.ac_sign_in;
+    }
+
+    @Override
+    public void initView(@Nullable Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
         final EditText editEmail = (EditText) findViewById(R.id.editEmail);
         final EditText editPassword = (EditText) findViewById(R.id.editPassword);
 
