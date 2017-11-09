e5e9ad01182761629301a9356ee1b626a794b698
chengxin <chengx@hlsteel.com>
2017-11-09 21:05:23 +0800

    添加注释

--- library/src/main/java/com/xcheng/permission/PermissionFragment.java
+++ library/src/main/java/com/xcheng/permission/PermissionFragment.java
@@ -14,6 +14,8 @@ import java.util.List;
 public class PermissionFragment extends Fragment {
     // Contains all the current permission requests.
     // Once granted or denied, they are removed from it.
+    //如果在发起请求时，Activity重启，之前的请求回调会丢失这是采用这种调用方法无法避免的，用户重新手动点击请求就OK了。
+    // 除非手动在onRequestPermissionsResult的回调方法里处理，但是那样整个请求流程会很麻烦，而且必须依赖在Activity和Fragment内部处理
     private final SparseArray<PermissionRequest> mPermissionRequests = new SparseArray<>();
 
     @TargetApi(Build.VERSION_CODES.M)
