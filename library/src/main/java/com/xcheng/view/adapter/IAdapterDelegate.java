875170b3119f26771b39a470cdb8de686325fea9
chengxin <chengx@hlsteel.com>
2017-11-20 15:03:59 +0800

    设置接口包内私有

--- library/src/main/java/com/xcheng/view/adapter/IAdapterDelegate.java
+++ library/src/main/java/com/xcheng/view/adapter/IAdapterDelegate.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
  * 抽离Adapter适配器接口
  * Created by chengxin on 2017/9/5.
  */
-public interface IAdapterDelegate<T, VH extends RecyclerView.ViewHolder> {
+interface IAdapterDelegate<T, VH extends RecyclerView.ViewHolder> {
     /**
      * 获取 itemView布局，这里不直接用getLayoutId的好处是可以兼容在代码里面构建的View，
      * 而且可以做一些不用重复初始化的设置，如设置背景、字体、监听等，
