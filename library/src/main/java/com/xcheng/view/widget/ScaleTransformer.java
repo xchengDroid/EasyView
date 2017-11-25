8af7f4c27c3e3486cb90e141a433972d8a915023
chengxin <chengx@hlsteel.com>
2017-11-25 10:02:09 +0800

    添加GalleryManager

---
+++ library/src/main/java/com/xcheng/view/widget/ScaleTransformer.java
@@ -0,0 +1,20 @@
+package com.xcheng.view.widget;
+
+import android.view.View;
+
+/**
+ * Created by chensuilun on 2016/12/16.
+ */
+public class ScaleTransformer implements GalleryLayoutManager.ItemTransformer {
+
+    private static final String TAG = "CurveTransformer";
+
+    @Override
+    public void transformItem(GalleryLayoutManager layoutManager, View item, float fraction) {
+        item.setPivotX(item.getWidth() / 2.f);
+        item.setPivotY(item.getHeight() / 2.0f);
+        float scale = 1 - 0.2f * Math.abs(fraction);
+        item.setScaleX(scale);
+        item.setScaleY(scale);
+    }
+}
