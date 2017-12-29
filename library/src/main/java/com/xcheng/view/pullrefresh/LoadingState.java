43ce5cbbdaa036d19c0c36bad3cd03bc9cb24e44
chengxin <chengx@hlsteel.com>
2017-12-29 12:43:15 +0800

    添加注释

--- library/src/main/java/com/xcheng/view/pullrefresh/LoadingState.java
+++ library/src/main/java/com/xcheng/view/pullrefresh/LoadingState.java
@@ -4,7 +4,7 @@ package com.xcheng.view.pullrefresh;
  * 刷新加载组件的状态
  */
 public enum LoadingState {
-    //可以刷新可加载更多
+    //可以刷新和加载更多
     INIT("上拉加载更多"),
     //刷新和加载更多有某一项正在进行
     REFRESHING("正在刷新"),
