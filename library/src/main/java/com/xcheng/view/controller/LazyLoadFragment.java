c7eb24501b6084142fec2c0a8949dfaf5f9a33d9
chengxin <chengx@hlsteel.com>
2018-01-09 14:29:02 +0800

    重写懒加载方法

---
+++ library/src/main/java/com/xcheng/view/controller/LazyLoadFragment.java
@@ -0,0 +1,49 @@
+package com.xcheng.view.controller;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+/**
+ * 懒加载Fragment,当对用用户可见的时候调用
+ */
+public abstract class LazyLoadFragment extends EasyFragment {
+    /**
+     * view是否已经初始化
+     */
+    private boolean mHasInitView;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mHasInitView = true;
+        if (canLazyLoad()) {
+            onLazyLoad();
+        }
+    }
+
+    protected abstract void onLazyLoad();
+
+    /**
+     * 如果需要过滤更多条件请重写此方法
+     *
+     * @return true 视图已经初始化并对用户可见,false others
+     */
+    protected boolean canLazyLoad() {
+        return mHasInitView && getUserVisibleHint();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (canLazyLoad()) {
+            onLazyLoad();
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mHasInitView = false;
+    }
+}
