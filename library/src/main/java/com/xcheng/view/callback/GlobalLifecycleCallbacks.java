fa626b3e5dae98c54e7e5d9cf0a1961f9aec2e1f
chengxin <chengx@hlsteel.com>
2018-01-17 14:14:13 +0800

    优化生命周期监听

--- library/src/main/java/com/xcheng/view/callback/GlobalLifecycleCallbacks.java
+++ library/src/main/java/com/xcheng/view/callback/GlobalLifecycleCallbacks.java
@@ -3,16 +3,25 @@ package com.xcheng.view.callback;
 import android.app.Activity;
 import android.app.Application.ActivityLifecycleCallbacks;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 /**
+ * 全局 Activity和Fragment 生命周期监听
  * Created by chengxin on 2017/9/29.
  */
-public class SimpleActivityLifecycleCallbacks implements ActivityLifecycleCallbacks {
-    String TAG = "ActivityLifecycleCallbacks";
+public class GlobalLifecycleCallbacks extends FragmentManager.FragmentLifecycleCallbacks implements ActivityLifecycleCallbacks {
+    static String TAG = "GlobalLifecycleCallbacks";
 
+    @CallSuper
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
+        if (activity instanceof FragmentActivity) {
+            //此处为Fragment注册生命周期，并且只有一个监听实例
+            ((FragmentActivity) activity).getSupportFragmentManager()
+                    .registerFragmentLifecycleCallbacks(this, true);
+        }
     }
 
     @Override
