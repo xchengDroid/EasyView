55840717762299afc8529609e6f951500c8f1972
chengxin <chengx@hlsteel.com>
2018-03-31 13:15:17 +0800

    修改为包内access

--- library/src/main/java/com/xcheng/view/Config.java
+++ library/src/main/java/com/xcheng/view/Config.java
@@ -14,7 +14,7 @@ import com.xcheng.view.util.EasyPreconditions;
  * 功能描述：EasyView全局参数配置类
  */
 public class Config {
-    public static final Factory DEFAULT_FACTORY = new Factory() {
+    static final Factory DEFAULT_FACTORY = new Factory() {
         @NonNull
         @Override
         public Dialog createLoadingDialog(Context context, String fromClazz) {
