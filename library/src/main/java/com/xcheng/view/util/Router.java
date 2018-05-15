a0f5d7c6dc2880a5e543e56d8d4bfbb55f4c802e
chengxin <chengx@hlsteel.com>
2018-05-15 11:34:04 +0800

    非空判断

--- library/src/main/java/com/xcheng/view/util/Router.java
+++ library/src/main/java/com/xcheng/view/util/Router.java
@@ -207,7 +207,7 @@ public final class Router {
     @Override
     public String toString() {
         return "Router{clazz="
-                + mClazz.getName()
+                + (mClazz != null ? mClazz.getName() : "null")
                 + ", className="
                 + mClassName
                 + ", action="
