d042b5d121ef78a52a904207155cef993fb2594a
chengxin <chengx@hlsteel.com>
2018-08-09 15:03:28 +0800

    添加    api 'com.github.GrenderG:Toasty:1.3.0'

--- app/src/main/java/com/simple/view/MessageActivity.java
+++ app/src/main/java/com/simple/view/MessageActivity.java
@@ -1,20 +1,20 @@
 package com.simple.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.EditText;
 
 import com.xcheng.view.controller.EasyActivity;
 
+import es.dmoral.toasty.Toasty;
 
-public class MessageActivity extends EasyActivity  {
+public class MessageActivity extends EasyActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_message);
-
-        final EditText editMessage = (EditText) findViewById(R.id.editMessage);
-
+        Toasty.Config.getInstance().setWarningColor(Color.RED).apply();
+        Toasty.info(this, "测试一般信息", 1, false).show();
     }
 
     @Override
