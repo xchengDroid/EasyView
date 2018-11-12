beb0db4375c9b9db51a5732192f1a86b8f6b6bea
chengxin <chengx@hlsteel.com>
2018-11-12 10:29:29 +0800

    review

--- library/src/main/java/com/xcheng/view/autosize/AutoSize.java
+++ library/src/main/java/com/xcheng/view/autosize/AutoSize.java
@@ -3,7 +3,9 @@ package com.xcheng.view.autosize;
 /**
  * 创建时间：2018/11/12
  * 编写人： chengxin
- * 功能描述：今日头条适配方案
+ * 功能描述：今日头条适配方案 https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA
  */
 public class AutoSize {
+
+
 }
