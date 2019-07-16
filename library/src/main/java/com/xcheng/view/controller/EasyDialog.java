2a21fa113f8533d99958308c99514260c1c5a432
chengxin <chengx@hlsteel.com>
2019-07-16 15:09:29 +0800

    删除不用的代码

--- library/src/main/java/com/xcheng/view/controller/EasyDialog.java
+++ library/src/main/java/com/xcheng/view/controller/EasyDialog.java
@@ -1,9 +1,7 @@
 package com.xcheng.view.controller;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.ContextWrapper;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,7 +14,7 @@ import com.xcheng.view.EasyView;
 
 /**
  * 基础的Dialog
- * Created by cc on 2016/11/7.
+ * Created by chengxin on 2016/11/7.
  */
 public abstract class EasyDialog extends Dialog implements IEasyView {
 
@@ -31,18 +29,6 @@ public abstract class EasyDialog extends Dialog implements IEasyView {
     public EasyDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
         super(context, cancelable, cancelListener);
     }
-
-    public Activity getActivity() {
-        Context context = getContext();
-        if (context instanceof Activity) {
-            return Activity.class.cast(context);
-        }
-        if (context instanceof ContextWrapper) {
-            context = ((ContextWrapper) context).getBaseContext();
-        }
-        return Activity.class.cast(context);
-    }
-
 
     @Override
     protected final void onCreate(Bundle savedInstanceState) {
